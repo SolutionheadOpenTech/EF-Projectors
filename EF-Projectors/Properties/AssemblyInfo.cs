d7681c8dcfdc3f54b1708b66795f9755e82b7646
rissaman <romanissa@hotmail.com>
2015-01-14 10:20:34 -0700

    Modified assembly info and nuspec.

--- EF-Projectors/Properties/AssemblyInfo.cs
+++ EF-Projectors/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EF-Projectors")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Facilitates creation of composable expressions for data projections.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Solutionhead Technologies, LLC")]
 [assembly: AssemblyProduct("EF-Projectors")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
